2d3834af6249d2db4a2e2e9d5abdc49c67267e0d
osvaldoml <miranda.osvaldo@gmail.com>
2023-02-27 19:21:23 -0300

    creando index.ejs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express from 'express'
-import {dirname} from 'path'
+import {dirname, join} from 'path'
 import { fileURLToPath} from 'url'
 
 const app = express()
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-console.log(__dirname)
+//console.log(__dirname, '/views')
+console.log(join(__dirname, 'views'))
 
-app.set('views', )
+app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
