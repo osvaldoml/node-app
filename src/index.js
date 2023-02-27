44e8278bb135248b0fe9de68446b641a18e2582d
osvaldoml <miranda.osvaldo@gmail.com>
2023-02-27 19:07:16 -0300

    nuevo index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+import express from 'express'
+import {dirname} from 'path'
+import { fileURLToPath} from 'url'
+
+const app = express()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname)
+
+app.set('views', )
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
+
+app.listen(3000)
+console.log('server is listening on port', 3000)
