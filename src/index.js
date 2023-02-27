5f8b97d9ad9be9bd78b3f7d3ff81ba6361db8409
osvaldoml <miranda.osvaldo@gmail.com>
2023-02-27 20:11:52 -0300

    avanzando

--- src/index.js
+++ src/index.js
@@ -13,5 +13,9 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
 
+app.get('/about', (req, res) => res.render('about'))
+
+app.get('/contact', (req, res) => res.render('contact'))
+
 app.listen(3000)
 console.log('server is listening on port', 3000)
